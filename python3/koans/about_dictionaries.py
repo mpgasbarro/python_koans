340601f1bf6ec382cc41a19113bc5f845c191b48
Gaspy <mpgasbarro@gmail.com>
2020-08-25 21:13:03 -0400

    Problem 45 solved

--- python3/koans/about_dictionaries.py
+++ python3/koans/about_dictionaries.py
@@ -35,8 +35,8 @@ class AboutDictionaries(Koan):
     def test_dictionary_is_unordered(self):
         dict1 = { 'one': 'uno', 'two': 'dos' }
         dict2 = { 'two': 'dos', 'one': 'uno' }
-
-        self.assertEqual(__, dict1 == dict2)
+        sorted(dict2.values())
+        self.assertEqual(True, dict1 == dict2)
 
 
     def test_dictionary_keys_and_values(self):
