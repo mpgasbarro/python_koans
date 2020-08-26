dccc0cc65fe5b8ddec9973c0b200352acde48f55
Gaspy <mpgasbarro@gmail.com>
2020-08-25 20:19:43 -0400

    Problem 32 solved

--- python3/koans/about_lists.py
+++ python3/koans/about_lists.py
@@ -58,11 +58,11 @@ class AboutLists(Koan):
         self.assertEqual([5,6,7,8], list(range(5, 9)))
 
     def test_ranges_with_steps(self):
-        self.assertEqual(__, list(range(5, 3, -1)))
-        self.assertEqual(__, list(range(0, 8, 2)))
-        self.assertEqual(__, list(range(1, 8, 3)))
-        self.assertEqual(__, list(range(5, -7, -4)))
-        self.assertEqual(__, list(range(5, -8, -4)))
+        self.assertEqual([5,4], list(range(5, 3, -1)))
+        self.assertEqual([0,2,4,6], list(range(0, 8, 2)))
+        self.assertEqual([1,4,7], list(range(1, 8, 3)))
+        self.assertEqual([5, 1, -3], list(range(5, -7, -4)))
+        self.assertEqual([5,1, -3, -7], list(range(5, -8, -4)))
 
     def test_insertions(self):
         knight = ['you', 'shall', 'pass']
